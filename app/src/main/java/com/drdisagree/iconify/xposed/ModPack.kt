4fb4fba44cfc8d7b3b51304fa26e0b909cc5b312
DrDisagree <crazymahmud08@gmail.com>
2025-01-19 11:06:28 +0600

    Use getter property for xposed mod context
    Signed-off-by: DrDisagree <crazymahmud08@gmail.com>

--- app/src/main/java/com/drdisagree/iconify/xposed/ModPack.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/ModPack.kt
@@ -3,7 +3,9 @@ package com.drdisagree.iconify.xposed
 import android.content.Context
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 
-abstract class ModPack(protected var mContext: Context) {
+abstract class ModPack(private var context: Context) {
+    protected val mContext: Context get() = context
+
     abstract fun updatePrefs(vararg key: String)
 
     abstract fun handleLoadPackage(loadPackageParam: LoadPackageParam)
