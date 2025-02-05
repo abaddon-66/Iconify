914ed98063a11fcb9ccba78b04b73a36452fcdfa
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-02-05 19:41:26 +0600

    Keep post-exec.sh on app update
    Signed-off-by: DrDisagree <29881338+Mahmud0808@users.noreply.github.com>

--- app/src/main/java/com/drdisagree/iconify/utils/helper/BackupRestore.kt
+++ app/src/main/java/com/drdisagree/iconify/utils/helper/BackupRestore.kt
@@ -1,6 +1,10 @@
 package com.drdisagree.iconify.utils.helper
 
-import com.drdisagree.iconify.common.Resources
+import com.drdisagree.iconify.common.Resources.BACKUP_DIR
+import com.drdisagree.iconify.common.Resources.MODULE_DIR
+import com.drdisagree.iconify.common.Resources.OVERLAY_DIR
+import com.drdisagree.iconify.common.Resources.TEMP_MODULE_DIR
+import com.drdisagree.iconify.common.Resources.TEMP_MODULE_OVERLAY_DIR
 import com.drdisagree.iconify.utils.RootUtils
 import com.topjohnwu.superuser.Shell
 
@@ -8,55 +12,57 @@ object BackupRestore {
 
     fun backupFiles() {
         // Create backup directory
-        Shell.cmd("rm -rf " + Resources.BACKUP_DIR, "mkdir -p " + Resources.BACKUP_DIR).exec()
+        Shell.cmd("rm -rf $BACKUP_DIR", "mkdir -p $BACKUP_DIR").exec()
 
-        backupFile(Resources.MODULE_DIR + "/system.prop")
-        backupFile(Resources.OVERLAY_DIR + "/IconifyComponentCR1.apk")
-        backupFile(Resources.OVERLAY_DIR + "/IconifyComponentCR2.apk")
-        backupFile(Resources.OVERLAY_DIR + "/IconifyComponentSIS.apk")
-        backupFile(Resources.OVERLAY_DIR + "/IconifyComponentSIP1.apk")
-        backupFile(Resources.OVERLAY_DIR + "/IconifyComponentSIP2.apk")
-        backupFile(Resources.OVERLAY_DIR + "/IconifyComponentSIP3.apk")
-        backupFile(Resources.OVERLAY_DIR + "/IconifyComponentPGB.apk")
-        backupFile(Resources.OVERLAY_DIR + "/IconifyComponentSWITCH1.apk")
-        backupFile(Resources.OVERLAY_DIR + "/IconifyComponentSWITCH2.apk")
-        backupFile(Resources.OVERLAY_DIR + "/IconifyComponentDynamic1.apk")
-        backupFile(Resources.OVERLAY_DIR + "/IconifyComponentDynamic2.apk")
+        backupFile("$MODULE_DIR/system.prop")
+        backupFile("$MODULE_DIR/post-exec.sh")
+        backupFile("$OVERLAY_DIR/IconifyComponentCR1.apk")
+        backupFile("$OVERLAY_DIR/IconifyComponentCR2.apk")
+        backupFile("$OVERLAY_DIR/IconifyComponentSIS.apk")
+        backupFile("$OVERLAY_DIR/IconifyComponentSIP1.apk")
+        backupFile("$OVERLAY_DIR/IconifyComponentSIP2.apk")
+        backupFile("$OVERLAY_DIR/IconifyComponentSIP3.apk")
+        backupFile("$OVERLAY_DIR/IconifyComponentPGB.apk")
+        backupFile("$OVERLAY_DIR/IconifyComponentSWITCH1.apk")
+        backupFile("$OVERLAY_DIR/IconifyComponentSWITCH2.apk")
+        backupFile("$OVERLAY_DIR/IconifyComponentDynamic1.apk")
+        backupFile("$OVERLAY_DIR/IconifyComponentDynamic2.apk")
     }
 
     fun restoreFiles() {
-        restoreFile("system.prop", Resources.TEMP_MODULE_DIR)
-        restoreFile("IconifyComponentCR1.apk", Resources.TEMP_MODULE_OVERLAY_DIR)
-        restoreFile("IconifyComponentCR2.apk", Resources.TEMP_MODULE_OVERLAY_DIR)
-        restoreFile("IconifyComponentSIS.apk", Resources.TEMP_MODULE_OVERLAY_DIR)
-        restoreFile("IconifyComponentSIP1.apk", Resources.TEMP_MODULE_OVERLAY_DIR)
-        restoreFile("IconifyComponentSIP2.apk", Resources.TEMP_MODULE_OVERLAY_DIR)
-        restoreFile("IconifyComponentSIP3.apk", Resources.TEMP_MODULE_OVERLAY_DIR)
-        restoreFile("IconifyComponentPGB.apk", Resources.TEMP_MODULE_OVERLAY_DIR)
-        restoreFile("IconifyComponentSWITCH1.apk", Resources.TEMP_MODULE_OVERLAY_DIR)
-        restoreFile("IconifyComponentSWITCH2.apk", Resources.TEMP_MODULE_OVERLAY_DIR)
-        restoreFile("IconifyComponentDynamic1.apk", Resources.TEMP_MODULE_OVERLAY_DIR)
-        restoreFile("IconifyComponentDynamic2.apk", Resources.TEMP_MODULE_OVERLAY_DIR)
+        restoreFile("system.prop", TEMP_MODULE_DIR)
+        restoreFile("post-exec.sh", TEMP_MODULE_DIR)
+        restoreFile("IconifyComponentCR1.apk", TEMP_MODULE_OVERLAY_DIR)
+        restoreFile("IconifyComponentCR2.apk", TEMP_MODULE_OVERLAY_DIR)
+        restoreFile("IconifyComponentSIS.apk", TEMP_MODULE_OVERLAY_DIR)
+        restoreFile("IconifyComponentSIP1.apk", TEMP_MODULE_OVERLAY_DIR)
+        restoreFile("IconifyComponentSIP2.apk", TEMP_MODULE_OVERLAY_DIR)
+        restoreFile("IconifyComponentSIP3.apk", TEMP_MODULE_OVERLAY_DIR)
+        restoreFile("IconifyComponentPGB.apk", TEMP_MODULE_OVERLAY_DIR)
+        restoreFile("IconifyComponentSWITCH1.apk", TEMP_MODULE_OVERLAY_DIR)
+        restoreFile("IconifyComponentSWITCH2.apk", TEMP_MODULE_OVERLAY_DIR)
+        restoreFile("IconifyComponentDynamic1.apk", TEMP_MODULE_OVERLAY_DIR)
+        restoreFile("IconifyComponentDynamic2.apk", TEMP_MODULE_OVERLAY_DIR)
 
         restoreBlurSettings()
 
         // Remove backup directory
-        Shell.cmd("rm -rf " + Resources.BACKUP_DIR).exec()
+        Shell.cmd("rm -rf $BACKUP_DIR").exec()
     }
 
     private fun backupExists(fileName: String): Boolean {
-        return RootUtils.fileExists(Resources.BACKUP_DIR + "/" + fileName)
+        return RootUtils.fileExists("$BACKUP_DIR/$fileName")
     }
 
     private fun backupFile(source: String) {
-        if (RootUtils.fileExists(source)) Shell.cmd("cp -rf " + source + " " + Resources.BACKUP_DIR + "/")
+        if (RootUtils.fileExists(source)) Shell.cmd("cp -rf $source $BACKUP_DIR/")
             .exec()
     }
 
     private fun restoreFile(fileName: String, dest: String) {
         if (backupExists(fileName)) {
             Shell.cmd("rm -rf $dest/$fileName").exec()
-            Shell.cmd("cp -rf " + Resources.BACKUP_DIR + "/" + fileName + " " + dest + "/").exec()
+            Shell.cmd("cp -rf $BACKUP_DIR/$fileName $dest/").exec()
         }
     }
 
@@ -69,15 +75,15 @@ object BackupRestore {
     private val isBlurEnabled: Boolean
         get() {
             val outs =
-                Shell.cmd("if grep -q \"ro.surface_flinger.supports_background_blur=1\" " + Resources.TEMP_MODULE_DIR + "/system.prop; then echo yes; else echo no; fi")
+                Shell.cmd("if grep -q \"ro.surface_flinger.supports_background_blur=1\" $TEMP_MODULE_DIR/system.prop; then echo yes; else echo no; fi")
                     .exec().out
             return outs[0] == "yes"
         }
 
     private fun disableBlur() {
-        Shell.cmd("mv " + Resources.TEMP_MODULE_DIR + "/system.prop " + Resources.TEMP_MODULE_DIR + "/system.txt; grep -v \"ro.surface_flinger.supports_background_blur\" " + Resources.TEMP_MODULE_DIR + "/system.txt > " + Resources.TEMP_MODULE_DIR + "/system.txt.tmp; rm -rf " + Resources.TEMP_MODULE_DIR + "/system.prop; mv " + Resources.TEMP_MODULE_DIR + "/system.txt.tmp " + Resources.TEMP_MODULE_DIR + "/system.prop; rm -rf " + Resources.TEMP_MODULE_DIR + "/system.txt; rm -rf " + Resources.TEMP_MODULE_DIR + "/system.txt.tmp")
+        Shell.cmd("mv $TEMP_MODULE_DIR/system.prop $TEMP_MODULE_DIR/system.txt; grep -v \"ro.surface_flinger.supports_background_blur\" $TEMP_MODULE_DIR/system.txt > $TEMP_MODULE_DIR/system.txt.tmp; rm -rf $TEMP_MODULE_DIR/system.prop; mv $TEMP_MODULE_DIR/system.txt.tmp $TEMP_MODULE_DIR/system.prop; rm -rf $TEMP_MODULE_DIR/system.txt; rm -rf $TEMP_MODULE_DIR/system.txt.tmp")
             .exec()
-        Shell.cmd("grep -v \"ro.surface_flinger.supports_background_blur\" " + Resources.TEMP_MODULE_DIR + "/service.sh > " + Resources.TEMP_MODULE_DIR + "/service.sh.tmp && mv " + Resources.TEMP_MODULE_DIR + "/service.sh.tmp " + Resources.TEMP_MODULE_DIR + "/service.sh")
+        Shell.cmd("grep -v \"ro.surface_flinger.supports_background_blur\" $TEMP_MODULE_DIR/service.sh > $TEMP_MODULE_DIR/service.sh.tmp && mv $TEMP_MODULE_DIR/service.sh.tmp $TEMP_MODULE_DIR/service.sh")
             .exec()
     }
 
@@ -88,9 +94,9 @@ object BackupRestore {
         val blurCmd2 =
             "resetprop ro.surface_flinger.supports_background_blur 1 && killall surfaceflinger"
 
-        Shell.cmd("echo \"" + blurCmd1 + "\" >> " + Resources.TEMP_MODULE_DIR + "/system.prop")
+        Shell.cmd("echo \"$blurCmd1\" >> $TEMP_MODULE_DIR/system.prop")
             .exec()
-        Shell.cmd("sed '/*}/a " + blurCmd2 + "' " + Resources.TEMP_MODULE_DIR + "/service.sh > " + Resources.TEMP_MODULE_DIR + "/service.sh.tmp && mv " + Resources.TEMP_MODULE_DIR + "/service.sh.tmp " + Resources.TEMP_MODULE_DIR + "/service.sh")
+        Shell.cmd("sed '/*}/a $blurCmd2' $TEMP_MODULE_DIR/service.sh > $TEMP_MODULE_DIR/service.sh.tmp && mv $TEMP_MODULE_DIR/service.sh.tmp $TEMP_MODULE_DIR/service.sh")
             .exec()
     }
 }
