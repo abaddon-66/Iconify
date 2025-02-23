73c749873e21420d1b3f464fda3a92edaafb12de
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-02-24 00:14:52 +0600

    Bump version code
    Signed-off-by: DrDisagree <29881338+Mahmud0808@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.drdisagree.iconify"
         minSdk = 31
         targetSdk = 35
-        versionCode = 22
-        versionName = "7.0.0"
+        versionCode = 23
+        versionName = "7.1.0"
         setProperty("archivesBaseName", "Iconify v${defaultConfig.versionName}")
         buildConfigField("int", "MIN_SDK_VERSION", "$minSdk")
 
